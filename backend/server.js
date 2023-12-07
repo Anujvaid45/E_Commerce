1da041e0b19708abe826460abe2a52d022e77e7f
Anujvaid45 <vaidanuj80@gmail.com>
2023-12-07 18:50:07 +0530

    changes

--- backend/server.js
+++ backend/server.js
@@ -9,14 +9,13 @@ const categoryRoutes = require('./routes/categoryRoutes.js')
 const productRoutes = require('./routes/productRoutes.js')
 const app = express()
 
-app.use(cors(
-    {
-        origin: ["https://e-commerce-frontend-nine-alpha.vercel.app"],
-        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-        credentials: true,
-        optionsSuccessStatus: 204,
-    }
-));
+const corsOrigin ={
+    origin: ["https://e-commerce-frontend-nine-alpha.vercel.app"],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+    optionsSuccessStatus: 204
+}
+app.use(cors(corsOrigin));
 
 //middleware
 app.use(express.json())
