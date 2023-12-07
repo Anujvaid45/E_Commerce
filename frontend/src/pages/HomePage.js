ab2d39d0cc11ffd6eed265b65cfd3256722cdf4d
Anujvaid45 <vaidanuj80@gmail.com>
2023-12-07 18:17:47 +0530

    change again!

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -18,6 +18,8 @@ const HomePage = () => {
     const navigate = useNavigate()
     const [cart,setCart] = useCart()
 
+    axios.defaults.withCredentials = true;
+
     //get Total count of products
     const getTotal = async()=>{
       try {
@@ -38,7 +40,7 @@ const HomePage = () => {
     const loadMore = async () => {
       try {
         setLoading(true);
-        const { data } = await axios.get(`/api/v1/product/product-list/${page}`);
+        const { data } = await axios.get(`https://e-commerce-backend-lovat.vercel.app/product/product-list/${page}`);
         setLoading(false);
         setProducts([...products, ...data?.products]);
       } catch (error) {
