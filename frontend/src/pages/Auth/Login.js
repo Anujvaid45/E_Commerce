216b48f06a54dccb65be72fb958adf5e26c0a33a
Anujvaid45 <vaidanuj80@gmail.com>
2023-12-09 14:50:52 +0530

    changes final

--- frontend/src/pages/Auth/Login.js
+++ frontend/src/pages/Auth/Login.js
@@ -16,7 +16,7 @@ const Login = () => {
     const handleSubmit = async (e) =>{
         e.preventDefault();
         try {
-            const res = await axios.post('https://e-commerce-backend-pl30.onrender.com/api/v1/auth/login',
+            const res = await axios.post('/api/v1/auth/login',
             {email,password}
             );
             if(res && res.data.success){
